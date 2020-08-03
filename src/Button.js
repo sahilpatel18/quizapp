4d36e525687459923ede0d9563445941b26d6fd7
sahilpatel18 <sahil.41801@gmail.com>
2020-08-03 16:55:27 -0400

    reusable button component

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = ({ onClick, className, name }) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {name}
+    </button>
+  );
+};
