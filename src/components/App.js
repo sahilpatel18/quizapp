24527259ba17e540509b752e1e0e8308ffd8c869
sahilpatel18 <sahil.41801@gmail.com>
2020-08-05 16:07:03 -0400

    removed login from app.js and during refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import bank from "../bank";
-import QuestionCard from "./QuestionCard";
+import Quiz from "./Quiz";
 import { shuffle } from "../utils";
+
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-
-
-  const tenRandomQuestions = bank.sort(() => 0.5 - Math.random());
-  let selected = tenRandomQuestions.slice(0, 10);
-
-  const randomQuestion = selected[Math.floor(Math.random() * selected.length)];
-
-  
-  const allAnswers = randomQuestion.all_answers;
-  const shuffledAnswers = shuffle(allAnswers);
-
-  const [score, setScore] = useState(0);
-  const [count, setCount] = useState(1);
-
+  const randomQuestions = shuffle(bank).slice(0, 10);
   return (
     <div>
       <ToastContainer />
-      <QuestionCard
-        question={randomQuestion}
-        shuffledAnswers={shuffledAnswers}
-        score={score}
-        setScore={setScore}
-        count={count}
-        setCount={setCount}
-      />
+      <Quiz randomQuestions={randomQuestions} />
     </div>
   );
 };
