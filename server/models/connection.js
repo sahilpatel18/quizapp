f269b86c34468e8fc12f19420ef2c379e4ce9014
sahilpatel18 <sahil.41801@gmail.com>
2020-08-10 11:56:03 -0400

    moving question bank data

--- server/models/connection.js
+++ server/models/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-// const Question = require('./question.model')
 
 
 mongoose.connect('mongodb+srv://sahil123:abc41801@quizappcluster.27keh.mongodb.net/quizapp?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
@@ -10,19 +9,3 @@ mongoose.connect('mongodb+srv://sahil123:abc41801@quizappcluster.27keh.mongodb.n
 require('./question.model')
 
 
-// const data = {
-//     difficulty: "hard",
-//     question: "What five letter word is the motto of the IBM Computer company?",
-//     correct_answers: ["Think"],
-//     all_answers: ["Click", "Logic", "Pixel", "Think"],
-// }
-
-// const newQuestion = new Question(data)
-
-// newQuestion.save((error) => {
-//     if(error){
-//         console.log('ERROR ERROR ERROR');
-//     } else {
-//         console.log('DATA HAS BEEN SAVED');
-//     }
-// })
