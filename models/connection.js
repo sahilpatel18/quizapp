a4fc044916bd7dfe3dbba9eaf880f78f3caf79dc
sahilpatel18 <sahil.41801@gmail.com>
2020-08-10 14:58:29 -0400

    add config to mongoDB constructor

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
+mongoose.connect(
+  "mongodb+srv://sahil123:abc41801@quizappcluster.27keh.mongodb.net/quizapp?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoDb connection Succeeded");
+    } else {
+      console.log("Error in DB connection :" + err);
+    }
+  }
+);
 
-mongoose.connect('mongodb+srv://sahil123:abc41801@quizappcluster.27keh.mongodb.net/quizapp?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
-    if(!err) {console.log('MongoDb connection Succeeded')}
-    else {console.log('Error in DB connection :' + err)}
-})
-
-require('./question.model')
-
-
+require("./question.model");
